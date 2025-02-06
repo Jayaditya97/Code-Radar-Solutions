b0c7916783e129dbb34e8a1450275c1154ac04cf
Jayaditya97 <jayadityanagpal@gmail.com>
2025-02-06 22:48:59 +0530

    Solution to Toggle nth Bit of a Number

--- Toggle nth Bit of a Number@@67882a731b4392d8fc1577ed/code.c
+++ Toggle nth Bit of a Number@@67882a731b4392d8fc1577ed/code.c
@@ -2,18 +2,13 @@
 
 int main() {
     int num1, num2;
-    
     // Input the number and the bit position
     scanf("%d %d", &num1, &num2);
-    
     // Create a mask by shifting 1 to the nth position
     int mask = 1 << num2;
-    
     // oggle Tthe nth bit using XOR
     num1 = num1 ^ mask;
-    
     // Output the updated number
     printf("%d\n", num1);
-    
     return 0;
 }
