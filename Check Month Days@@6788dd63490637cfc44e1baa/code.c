bba0df452cdaa5a5700dff08dd500c02e57f2d9a
Jayaditya97 <jayadityanagpal@gmail.com>
2025-03-10 22:05:24 +0530

    Solution to Check Month Days

--- Check Month Days@@6788dd63490637cfc44e1baa/code.c
+++ Check Month Days@@6788dd63490637cfc44e1baa/code.c
@@ -1,49 +1,51 @@
 #include <stdio.h>
-int main(){
+
+int main() {
     int month;
     scanf("%d", &month);
-    if (month<=0 || month>12){
-        printf("Invalid month");
-    }
-    else{
-        switch(month){
-            case '1':
-            printf("31");
-            break;
-            case '2':
-            printf("28");
-            break;
-            case '3':
-            printf("31");
-            break;
-            case '4':
-            printf("30");
-            break;
-            case '5':
-            printf("31");
-            break;
-            case '6':
-            printf("30");
-            break;
-            case '7':
-            printf("31");
-            break;
-            case '8':
-            printf("30");
-            break;
-            case '9':
-            printf("31");
-            break;
-            case '10':
-            printf("31");
-            break;
-            case '11':
-            printf("30");
-            break;
-            case '12':
-            printf("31");
-            break;
+
+    if (month <= 0 || month > 12) {
+        printf("Invalid month\n");
+    } else {
+        switch (month) {
+            case 1:
+                printf("31\n");
+                break;
+            case 2:
+                printf("28\n");
+                break;
+            case 3:
+                printf("31\n");
+                break;
+            case 4:
+                printf("30\n");
+                break;
+            case 5:
+                printf("31\n");
+                break;
+            case 6:
+                printf("30\n");
+                break;
+            case 7:
+                printf("31\n");
+                break;
+            case 8:
+                printf("31\n");
+                break;
+            case 9:
+                printf("30\n");
+                break;
+            case 10:
+                printf("31\n");
+                break;
+            case 11:
+                printf("30\n");
+                break;
+            case 12:
+                printf("31\n");
+                break;
         }
     }
+
     return 0;
 }
