6229b0b593f441a81a52b2ac51497a340c17b07b
Jayaditya97 <jayadityanagpal@gmail.com>
2025-03-10 22:15:13 +0530

    Solution to Grade Evaluation

--- Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -9,14 +9,19 @@ int main(){
         switch(c){
             case 'A':
             printf("Excellent");
+            break;
             case 'B':
             printf("Good");
+            break;
             case 'C':
             printf("Average");
+            break;
             case 'D':
             printf("Below Average");
+            break;
             case 'F':
             printf("Fail");
+            break;
         }
     }
     return 0;
